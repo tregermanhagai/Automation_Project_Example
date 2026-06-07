ed806d55e0e4993d1d29b647fd9d89abd71e0a5f
hagai.tregerman <hagai1973@gmail.com>
2026-06-07 14:53:58 +0300

    improve example UI test

--- tests/ui/test_positive.py
+++ tests/ui/test_positive.py
@@ -1,5 +1,4 @@
 import os
-import time
 import pytest
 from dotenv import load_dotenv
 from playwright.sync_api import Page, expect
@@ -18,3 +17,5 @@ def test_login_success(page: Page):
     page.locator("[data-test='input-password']").fill(password)
     page.get_by_role("button", name="Sign In").click()
     expect(page).to_have_url(f"{BASE}/pages/home.html", timeout=15000)
+    expect(page.locator("[data-test='nav-system']")).to_be_visible()
+    expect(page.get_by_role("link", name="Logout")).to_be_visible()
