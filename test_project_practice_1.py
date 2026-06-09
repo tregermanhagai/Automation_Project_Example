00fc1c02ddb2aec63ee4a8eb0d77c6710226e021
hagai.tregerman <hagai1973@gmail.com>
2026-06-09 19:25:22 +0300

    Add .env using in the sample test

--- test_project_practice_1.py
+++ test_project_practice_1.py
@@ -1,14 +1,24 @@
 from playwright.sync_api import Page, expect
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE = os.getenv("BASE_URL", "")
+email = os.getenv("ADMIN_USER", "")
+password = os.getenv("PASSWORD", "")
+
 
 def test_validate_admin_have_system_options(page: Page):
     """As an admin I will expect that after login the system menu will be visable"""
     
-    page.goto("https://sv-students-recommend.onrender.com/")
-    page.locator("[data-test='input-email']").fill("hagai@svcollege.co.il")
-    page.locator("[data-test='input-password']").fill("test1234")
+    page.goto(BASE)
+    page.locator("[data-test='input-email']").fill(email)
+    page.locator("[data-test='input-password']").fill(password)
     page.get_by_role("button", name="Sign In").click()
     page.locator("[data-test='nav-system']").click()
     
-    expect(page).to_have_title("System – SV Students Recommend")
+    # expect(page).to_have_title("System – SV Students Recommend")
+    expect(page.get_by_role("heading", name="System Management")).to_be_visible
 
    
